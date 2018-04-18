1a9a74c38718cb3f351491d21540311a0d757b8b
DubeyJacob <35313774+DubeyJacob@users.noreply.github.com>
2018-04-18 09:05:15 -0400

    Update w/score

--- Blackhole.py
+++ Blackhole.py
@@ -2,62 +2,86 @@
 import sys, pygame, random, os
 assert sys.version_info >= (3,4), 'This script requires at least python 3.4'
 
-screen_size = (500,650)
+screen_size = (400,650)
 FPS = 60
 black = (0,0,0)
 white = (255,255,255)
 class Asteroid(pygame.sprite.Sprite):
-    def __init__(self, white, size, position, direction):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface(size)
-        self.image.fill(white)
-        self.rect = self.image.get_rect()
-        (self.rect.x,self.rect.y) = position
-        self.direction = direction
+	def __init__(self, white, size, position, direction):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface(size)
+		self.image.fill(white)
+		self.rect = self.image.get_rect()
+		(self.rect.x,self.rect.y) = position
+		self.direction = direction
 
-    def update(self):
-        (dx,dy) = self.direction
+	def update(self):
+		(dx,dy) = self.direction
 
-        self.rect.y += dy
+		self.rect.y += dy
+
+	def check_on_screen(self, screen_size):
+		if self.rect.top > screen_size[1]:
+			return False
+		return True
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode(screen_size)
-    screen_rect = screen.get_rect()
-    clock = pygame.time.Clock()
+	pygame.init()
+	screen = pygame.display.set_mode(screen_size)
+	screen_rect = screen.get_rect()
+	clock = pygame.time.Clock()
+	font = pygame.font.SysFont('arial', 24)
 
-    blocks = pygame.sprite.Group()
-    spritenum = 5
+	blocks = pygame.sprite.Group()
+	spritenum = 1
 
-    spritenum -= 1
-    s = 0
+	spritenum -= 1
+	s = 0
+	score = 0
 
 
-    while True:
-        while spritenum > 0:
-            color = white
+	color = white
 
-            randomposition = (random.randrange(screen_size[0]), 0)
-            ydirection = (0, 10)
-            size = (20, 20)
-            block = Asteroid(color, size, randomposition, ydirection)
-            blocks.add(block)
-            spritenum -= 1
-        s += 10
-        if s == 650:
-            spritenum = 1
-            s = 0
-        print(s)
+	randomposition = (random.randrange(screen_size[0]), 0)
+	ydirection = (random.randrange(4, 10), random.randrange(4, 10))
+	size = (20, 20)
+	block = Asteroid(color, size, randomposition, ydirection)
+	blocks.add(block)
+	block_list = []
+	block_list.append(block)
 
-        clock.tick(FPS)
-        screen.fill(black)
+	while True:
+		clock.tick(FPS)
+		screen.fill(black)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit(0)
-        blocks.update()
-        blocks.draw(screen)
-        pygame.display.flip()
+		for b in block_list:
+			if not b.check_on_screen(screen_size):
+				b.rect.bottom = 0
+				randomposition = (random.randrange(screen_size[0]), 0)
+				ydirection = (random.randrange(4, 10), random.randrange(4, 10))
+				size = (20, 20)
+				block = Asteroid(color, size, randomposition, ydirection)
+				blocks.add(block)
+				block_list.append(block)
+
+				s += 10
+		if s == 650:
+			spritenum = 1
+			s = 0
+
+		print(s)
+		text = "Score: {0}".format(score)
+		score += 1
+		f = font.render(text, True, white)
+		(fwidth, fheight) = font.size(text)
+		screen.blit(f, (5,5))
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit(0)
+		blocks.update()
+		blocks.draw(screen)
+		pygame.display.flip()
 if __name__ == '__main__':
-    main()
+	main()
---
+++ README.txt
@@ -0,0 +1,5 @@
+#Blackhole_Game_Jam
+
+Call it an arcade game if you want, focusing on survival, the game revolves around you and your fellow
+space traveller friends being out exploring the stars when suddenly you happen to pass into an asteroid field.
+Now instead of exploring you're trying to survive.
